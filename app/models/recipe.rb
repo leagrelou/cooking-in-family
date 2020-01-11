a3ce89b03a1aa632b501e7c9a05a6201c2e840e1
Lea Grelou <leagrelou@gmail.com>
2020-01-10 21:49:24 -0500

    ongoing creating of the mvc model for recipes

---
+++ app/models/recipe.rb
@@ -0,0 +1,29 @@
+class Recipe < ApplicationRecord
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :category, :instructions, :photo)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Recipe.new(name: "Tartiflette")
+Recipe.new(name: "Raclette")
+Recipe.new(name: "Soupe")
